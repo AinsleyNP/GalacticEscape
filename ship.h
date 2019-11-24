1180d74d76660de356e94291117841be7a3bd3c2
Ainsley Malcolm Pereira <s10186606@connect.np.edu.sg>
2019-11-24 17:32:15 +0800

    New Ship - Edited texture file

--- ship.h
+++ ship.h
@@ -21,7 +21,7 @@ namespace shipNS
     const float MASS = 300.0f;              // mass
     const int   TEXTURE_COLS = 8;           // texture has 8 columns
     const int   SHIP1_START_FRAME = 0;      // ship1 starts at frame 0
-    const int   SHIP1_END_FRAME = 3;        // ship1 animation frames 0,1,2,3
+    const int   SHIP1_END_FRAME = 0;        // ship1 animation frames 0,1,2,3
     const int   SHIP2_START_FRAME = 8;      // ship2 starts at frame 8
     const int   SHIP2_END_FRAME = 11;       // ship2 animation frames 8,9,10,11
     const float SHIP_ANIMATION_DELAY = 0.2f;    // time between frames
