1f2d89d95a0c21f72a0d60a9df1d946591b3b50d
Ainsley Malcolm Pereira <s10186606@connect.np.edu.sg>
2019-12-05 14:57:20 +0800

    Revert "merge: brannon local kept"
    This reverts commit ce18103315189ca60712a310a322cd6362ae0397, reversing
    changes made to 9ea6a29e7f3e814a1a04284dd535861eb0648cd7.

--- laser.cpp
+++ laser.cpp
@@ -38,7 +38,11 @@ bool Laser::initialize(Game* gamePtr, int width, int height, int ncols,
 //=============================================================================
 void Laser::draw()
 {
+	if (input->isKeyDown(VK_SPACE))
+	{
 		Image::draw();		           // draw laser
+	}
+
 }
 
 //=============================================================================
